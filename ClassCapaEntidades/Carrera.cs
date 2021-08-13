77527b015dbcfda502408172a4779a81e0b9b64f
Aldo Romano <aldoromanorojas@gmail.com>
2021-08-12 23:49:49 -0500

    Proyecto Inicial. Creación de la Capa Acceso a Datos y la Capa Entidades

---
+++ ClassCapaEntidades/Carrera.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class Carrera
+    {
+        public int Id_carrera { get; set; }
+        public string NombreCarrera { get; set; }
+
+    }
+}
---
+++ ClassCapaEntidades/ProgramaEducativo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class ProgramaEducativo
+    {
+        public int Id_pe { get; set; }
+        public string ProgramaEd { get; set; }
+        public int F_carrera { get; set; }
+        public string Extra { get; set; }
+
+    }
+}
