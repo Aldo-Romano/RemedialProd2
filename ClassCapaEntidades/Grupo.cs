a5415eeccdfbe1ed1cfc3091134a4732c057f4d8
Aldo Romano <aldoromanorojas@gmail.com>
2021-08-15 22:22:42 -0500

    Modificacion de la clase Grupo en la Capa Entidades

--- ClassCapaEntidades/Grupo.cs
+++ ClassCapaEntidades/Grupo.cs
@@ -6,7 +6,11 @@ using System.Threading.Tasks;
 
 namespace ClassCapaEntidades
 {
-    class Grupo
+    public class Grupo
     {
+        public int Id_Grupo { get; set; }
+        public byte Grado { get; set; }
+        public string Letra { get; set; }
+        public string Extra { get; set; }
     }
 }
