e69a95da33c2f2ef7d2831b849a5f37c7400778d
Aldo Romano <aldoromanorojas@gmail.com>
2021-08-15 20:50:14 -0500

    Se creo la rama AldoR2. Se creo la clase Materia en la capa de entidades. Se creo la LogicaNegociosMateria en la Capa Logica Negocios. Ya tiene los metodos de Insertar, Mostrar, Eliminar y Actualizar.

---
+++ ClassCapaEntidades/Materia.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassCapaEntidades
+{
+    public class Materia
+    {
+        public int Id_Materia { get; set; }
+        public string NombeMateria { get; set; }
+        public byte HorasSemana { get; set; }
+        public string Extra { get; set; }
+    }
+}
